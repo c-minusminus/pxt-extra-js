cedd1e1b61665b8d93f18ee4b29a8ee3d2572404
VoxelMaster64 <mcross.student@bccspa.org>
2026-08-29 09:11:57 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
- 
+namespace game {
+    /**
+     * Render the images of sprites
+     * @param body code to execute
+     */
+    //% group="Gameplay"
+    //% help=game/paint weight=10 afterOnStart=true
+    //% blockId=gamepaint block="on game paint"
+    //% blockAllowMultiple=1
+    declare function onPaint(a: () => void): void
+}
